ce82d76d8b96b21b4234793dff2b0f2bf9c339a2
ocavue <ocavue@users.noreply.github.com>
2021-09-04 21:41:12 +0000

    init

---
+++ imports/ui/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useRemirror } from '@remirror/react';
+
+console.log("useRemirror:", useRemirror)
+
+export const Hello = () => {
+  return (
+    <div>
+      TEST
+    </div>
+  );
+};
